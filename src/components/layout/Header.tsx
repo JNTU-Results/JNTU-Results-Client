af4914ef0ff8adeff7c926585a8de7e3ba9da8c4
Syed Ansar <syedansar@Syeds-MacBook-Air.local>
2023-05-02 23:28:40 +0530

    setup: frontend setup

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function Header() {
+  return (
+    <header className='sticky top-0 z-50 bg-white'>
+     <h1>Header</h1>
+    </header>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+import Layout from '@/components/layout/Layout';
+import Seo from '@/components/Seo';
+
+/**
+ * SVGR Support
+ * Caveat: No React Props Type.
+ *
+ * You can override the next-env if the type is important to you
+ * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
+ */
+import Vercel from '~/svg/Vercel.svg';
+
+// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
+// Before you begin editing, follow all comments with `STARTERCONF`,
+// to customize the default configuration.
+
+export default function HomePage() {
+  return (
+    <Layout>
+      {/* <Seo templateTitle='Home' /> */}
+      <Seo />
+
+      <main>
+        <h1>JNTUH Frontend</h1>
+      </main>
+    </Layout>
+  );
+}
